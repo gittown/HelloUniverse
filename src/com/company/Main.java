04fb1a32741910762e1a2af0e748b6ca65ec7693
gittown <gittown@gittownrovers.com>
2020-05-31 21:14:27 +0100

    Initial Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         log("Pi:"+Math.PI);
+        log("1");
     }
     static void log(String s){
         System.out.println(s);
