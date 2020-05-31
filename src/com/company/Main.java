7f26ddd640d2447dbb6512b8cada84e8d309b9d7
gittown <gittown@gittownrovers.com>
2020-05-31 21:01:00 +0100

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        log("Pi:"+Math.PI);
+    }
+    static void log(String s){
+        System.out.println(s);
+    }
+}
